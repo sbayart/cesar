6fa1383e08b7a8ff790e8863ff071311390d7aec
ThibDT <thibault.de.tourdonnet@gmail.com>
2017-02-13 15:35:36 +0100

    fin ?

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var textElement = document.getElementById("initial");
 var transform = document.getElementById("transform");
+var pasElement = document.getElementById("pas");
+
 function cripter() {
+    var pas = parseInt(pasElement.value);
     text = textElement.value;
     var resultat = "";
         for (var i = 0; i < text.length; i++) {
-            var indice = alphabet.indexOf(text[i])
-            resultat += alphabet[indice+1]
+            if(text[i] == " ") {
+                resultat += " ";
+            }
+            else {
+                var indice = alphabet.indexOf(text[i].toUpperCase());
+                var nouvelIndice = indice + pas;
+
+                resultat += alphabet[nouvelIndice % 26];
+            }
+
     }
     transform.value = resultat;
 }
